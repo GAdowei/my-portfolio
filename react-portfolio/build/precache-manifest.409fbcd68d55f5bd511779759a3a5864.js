576dcafcb45e3f3ddd37af98ffab1ae66310f6a2
GmanMcman <graciousadowei@gmail.com>
2020-10-01 01:28:24 +0100

    added production build

---
+++ react-portfolio/build/precache-manifest.409fbcd68d55f5bd511779759a3a5864.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda2321215dfdcdd1a91f4af584fa6db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d7250ce66af99237d38",
+    "url": "/static/css/main.0688f24f.chunk.css"
+  },
+  {
+    "revision": "0289bab34fe9eb580804",
+    "url": "/static/js/2.8e3c620c.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/static/js/2.8e3c620c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d7250ce66af99237d38",
+    "url": "/static/js/main.fb85dc36.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "3b6d3b9507cb6c9abe43b905b1c0953e",
+    "url": "/static/media/facebook-logo.3b6d3b95.png"
+  },
+  {
+    "revision": "d4c0c8348ba0e9ab025318d91772df4c",
+    "url": "/static/media/pinterest-logo-white.d4c0c834.png"
+  },
+  {
+    "revision": "777cfd4016fd54f1164ee95fd588e490",
+    "url": "/static/media/pokemon_logo.777cfd40.jpg"
+  },
+  {
+    "revision": "0ad1d17f18f25d10000c84bc82e7bd9f",
+    "url": "/static/media/react-icon.0ad1d17f.png"
+  }
+]);
